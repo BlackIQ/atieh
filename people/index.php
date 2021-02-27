9aaa4b72da8034555d18f63c850282bf12b85051
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-02-27 20:25:03 +0330

    Make headers

--- people/index.php
+++ people/index.php
@@ -1,3 +1,17 @@
 <?php
 
-header("Location: http://192.168.1.4/Atieh");
+$ip = "192.168.1.3";
+
+session_start();
+
+if ($_SESSION['status'] == true) {
+    if ($_SESSION['person'] == "student") {
+        header("Location: http://$ip/Atieh/people/student");
+    }
+    if ($_SESSION['person'] == "teacher") {
+        header("Location: http://$ip/Atieh/people/teacher");
+    }
+}
+else {
+    header("Location: http://$ip/Atieh");
+}
