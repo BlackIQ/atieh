c9b19a4c3163581c7ef3527c18ec5cbca60067f0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-02-27 16:07:23 +0330

    Put main files in student vendor

---
+++ people/teacher/vendor/panels/class.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+// Database Connection
+$server = "localhost";
+$user = "milad";
+$passwd = "milad";
+$db = "Atieh";
+
+$conn = mysqli_connect($server, $user, $passwd, $db);
+
+$tcode = $_SESSION['code'];
+
+$sql = "SELECT * FROM class WHERE teacher_code='$tcode'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+        <li>
+            <a class="" href=#">
+                <span class="fa fa-user">&nbsp;</span> <?php echo $row["level"]; ?>
+            </a>
+        </li>
+        <?php
+    }
+}
