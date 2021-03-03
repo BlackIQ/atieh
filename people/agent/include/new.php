ca9efcfacd71e4d9e64aaae371579ec7a796f1c3
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-04 02:00:20 +0330

    Make Redirect after posting

--- people/agent/include/new.php
+++ people/agent/include/new.php
@@ -12,6 +12,13 @@ $dbname = "Atieh";
 // Create connection
 $conn = mysqli_connect($servername, $user, $password, $dbname);
 
+$getip = "SELECT * FROM development";
+$res = mysqli_query($conn, $getip);
+
+while ($row = mysqli_fetch_assoc($res)) {
+    $ip = $row['ip'];
+}
+
 $titlw = $_POST['title'];
 $description = $_POST['description'];
 $date = date("M , d , Y");
@@ -21,3 +28,5 @@ $code = rand(1000 , 9999);
 $sql = "INSERT INTO post VALUES ('$code' , '$titlw' , '$description' , '$date' , '$who')";
 
 mysqli_query($conn, $sql);
+
+header("Location: http://$ip/Atieh/people/agent");
