f557d5ddb2af761b18ec6792b56247e953bcf587
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-04 15:46:22 +0330

    Change hrefs or src files address

--- people/agent/posts.php
+++ people/agent/posts.php
@@ -20,10 +20,10 @@ $result = mysqli_query($conn, $sql);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Atieh - Posts</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/datepicker3.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="../pack/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../pack/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../pack/css/datepicker3.css" rel="stylesheet">
+    <link href="../pack/css/styles.css" rel="stylesheet">
 
     <!--Custom Font-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
@@ -40,7 +40,7 @@ include("panels/sidebar.php");
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
-            <li><a href="../student">
+            <li><a href="../agent">
                     <em class="fa fa-home"></em>
                 </a></li>
             <li class="active">Posts</li>
@@ -113,14 +113,14 @@ include("panels/sidebar.php");
     ?>
 </div>    <!--/.main-->
 
-<script src="js/jquery-1.11.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/chart.min.js"></script>
-<script src="js/chart-data.js"></script>
-<script src="js/easypiechart.js"></script>
-<script src="js/easypiechart-data.js"></script>
-<script src="js/bootstrap-datepicker.js"></script>
-<script src="js/custom.js"></script>
+<script src="../pack/js/jquery-1.11.1.min.js"></script>
+<script src="../pack/js/bootstrap.min.js"></script>
+<script src="../pack/js/chart.min.js"></script>
+<script src="../pack/js/chart-data.js"></script>
+<script src="../pack/js/easypiechart.js"></script>
+<script src="../pack/js/easypiechart-data.js"></script>
+<script src="../pack/js/bootstrap-datepicker.js"></script>
+<script src="../pack/js/custom.js"></script>
 <script>
     window.onload = function () {
         var chart1 = document.getElementById("line-chart").getContext("2d");
