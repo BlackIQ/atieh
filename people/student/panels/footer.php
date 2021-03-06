c0418a028a55da78a4d04cb03acb82367a32fc45
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-06 23:11:04 +0330

    New footer for all

--- people/student/panels/footer.php
+++ people/student/panels/footer.php
@@ -1,12 +1,10 @@
 <div class="row">
     <div class="col-sm-12">
-        <p class="back-link">Designed by <a href="https://www.github.com/BlackIQ">Amirhossein Mohammadi</a> <i class="fa fa-heart color-red"></i></p>
-        <p class="back-link">Everything Belongs To <a href="https://www.github.com/BlackIQ">Atieh</a> <i class="fa fa-copyright"></i> 2021</p>
+        <p class="back-link">Created by <a href="https://www.github.com/BlackIQ">Amirhossein Mohammadi</a></p>
+        <p class="back-link">Powered By <a href="https://www.linkedin.com/company/neotrinost">Neotrinost</a> <i class="fa fa-copyright"></i> <?php echo date("Y"); ?></p>
         <p class="back-link">
             <i class="fa fa-telegram text-primary"></i>
             <i class="fa fa-instagram text-danger"></i>
-            <i class="fa fa-twitter text-primary"></i>
-            <i class="fa fa-facebook text-info"></i>
         </p>
     </div>
 </div><!--/.row-->
