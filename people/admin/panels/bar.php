2042b897ee570f778505f5b482257bb70d3baaf1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-11 22:32:08 +0330

    Panels for sidebar

---
+++ people/admin/panels/bar.php
@@ -0,0 +1,38 @@
+<div class="row">
+    <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="panel panel-teal panel-widget border-right">
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-blue"></em>
+                <div class="large"><?php echo $teachers; ?></div>
+                <br>
+                <div class="text-muted">Teachers</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="panel panel-blue panel-widget border-right">
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-orange"></em>
+                <div class="large"><?php echo $students; ?></div>
+                <br>
+                <div class="text-muted">Students</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="panel panel-orange panel-widget border-right">
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-red"></em>
+                <div class="large"><?php echo $agents; ?></div>
+                <br>
+                <div class="text-muted">Agents</div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="panel panel-red panel-widget ">
+            <div class="row no-padding"><em class="fa fa-xl fa-home color-gray"></em>
+                <div class="large"><?php echo $classes; ?></div>
+                <br>
+                <div class="text-muted">Classes</div>
+            </div>
+        </div>
+    </div>
+</div><!--/.row-->
