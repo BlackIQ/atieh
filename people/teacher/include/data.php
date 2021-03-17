c35ee1651123cfb2aeee8a092d93629cd51f1de0
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-17 19:45:17 +0330

    Change Name and Clean Code

--- people/teacher/include/data.php
+++ people/teacher/include/data.php
@@ -17,14 +17,6 @@ while ($row = mysqli_fetch_assoc($res)) {
 }
 
 if ($_SESSION['status'] == true && $_SESSION['person'] == "teacher") {
-    // Database Connection
-    $server = "localhost";
-    $user = "milad";
-    $passwd = "milad";
-    $db = "Atieh";
-
-    $conn = mysqli_connect($server, $user, $passwd, $db);
-
     $tcode = $_SESSION['code'];
 
     $sql = "SELECT * FROM teacher WHERE code='$tcode'";
@@ -39,37 +31,16 @@ if ($_SESSION['status'] == true && $_SESSION['person'] == "teacher") {
             $phone = $row["phone"];
         }
     }
-
-    $sql = "SELECT * FROM class WHERE teacher_code='$tcode'";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $code = $row["code"];
-            $teacher = $row["teacher"];
-            $starttime = $row["start_time"];
-            $endtime = $row["end_time"];
-            $whatapp = $row["whatsapp_link"];
-            $skype = $row["skype_link"];
-            $price = $row["tuition_price"];
-            $homeworksession = $row["homework_session"];
-            $homeworktitle = $row["homework_title"];
-            $homeworktext = $row["homework_text"];
-            $sex = $row['sex'];
-            $level = $row['level'];
-            $days = $row['days'];
-        }
-    }
 }
 elseif ($_SESSION['person'] == "student") {
-    header("Location: http://$ip/Atieh/people/student");
+    header("Location: http://$ip/Narbon/people/student");
 }
 elseif ($_SESSION['person'] == "agent") {
-    header("Location: http://$ip/Atieh/people/agent");
+    header("Location: http://$ip/Narbon/people/agent");
 }
 elseif ($_SESSION['person'] == "admin") {
-    header("Location: http://$ip/Atieh/people/admin");
+    header("Location: http://$ip/Narbon/people/admin");
 }
 else {
-    header("Location: http://$ip/Atieh");
+    header("Location: http://$ip/Narbon");
 }
