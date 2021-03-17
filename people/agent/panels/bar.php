a60baf826c28998962e80086479692274fa5bf20
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-17 19:54:23 +0330

    Working on bar

--- people/agent/panels/bar.php
+++ people/agent/panels/bar.php
@@ -1,37 +1,37 @@
 <div class="row">
     <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
         <div class="panel panel-teal panel-widget border-right">
-            <div class="row no-padding"><em class="fa fa-xl fa-money color-blue"></em>
-                <div class="large"><?php echo $payment; ?></div>
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-blue"></em>
+                <div class="large"><?php echo $teachers; ?></div>
                 <br>
-                <div class="text-muted">Payment Status</div>
+                <div class="text-muted">Teachers</div>
             </div>
         </div>
     </div>
     <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
         <div class="panel panel-blue panel-widget border-right">
-            <div class="row no-padding"><em class="fa fa-xl fa-clock-o color-orange"></em>
-                <div class="large"><?php echo $starttime; ?></div>
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-orange"></em>
+                <div class="large"><?php echo $students; ?></div>
                 <br>
-                <div class="text-muted">Start Time</div>
+                <div class="text-muted">Students</div>
             </div>
         </div>
     </div>
     <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
         <div class="panel panel-orange panel-widget border-right">
-            <div class="row no-padding"><em class="fa fa-xl fa-book color-teal"></em>
-                <div class="large"><?php echo $level; ?></div>
+            <div class="row no-padding"><em class="fa fa-xl fa-users color-red"></em>
+                <div class="large"><?php echo $agents; ?></div>
                 <br>
-                <div class="text-muted">Level</div>
+                <div class="text-muted">Agents</div>
             </div>
         </div>
     </div>
     <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
         <div class="panel panel-red panel-widget ">
-            <div class="row no-padding"><em class="fa fa-xl fa-user color-red"></em>
-                <div class="large"><?php echo $teacher; ?></div>
+            <div class="row no-padding"><em class="fa fa-xl fa-home color-gray"></em>
+                <div class="large"><?php echo $classes; ?></div>
                 <br>
-                <div class="text-muted">Teacher</div>
+                <div class="text-muted">Classes</div>
             </div>
         </div>
     </div>
