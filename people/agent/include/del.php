c5a68b741b63dc00c7aeee0b205248cfa7b29ef7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-17 19:53:00 +0330

    Change name and clean

--- people/agent/include/del.php
+++ people/agent/include/del.php
@@ -1,22 +1,10 @@
 <?php
 
+include("data.php");
+
 $code = $_GET['code'];
-
-$server = "localhost";
-$user = "milad";
-$passwd = "milad";
-$db = "Atieh";
-
-$conn = mysqli_connect($server, $user, $passwd, $db);
-
-$getip = "SELECT * FROM development";
-$res = mysqli_query($conn, $getip);
-
-while ($row = mysqli_fetch_assoc($res)) {
-    $ip = $row['ip'];
-}
 
 $sql = "DELETE FROM post WHERE code=$code";
 $result = mysqli_query($conn, $sql);
 
-header("Location: http://$ip/Atieh/people/agent/posts.php");
+header("Location: http://$ip/Narbon/people/agent/posts.php");
