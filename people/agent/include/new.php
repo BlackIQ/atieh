2d6ded29ba6754e42501cb75c8da41c584dce5da
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-17 19:53:11 +0330

    Change name and fix small issue

--- people/agent/include/new.php
+++ people/agent/include/new.php
@@ -4,37 +4,22 @@ session_start();
 
 include("data.php");
 
-$servername = "localhost";
-$user = "milad";
-$password = "milad";
-$dbname = "Atieh";
-
-// Create connection
-$conn = mysqli_connect($servername, $user, $password, $dbname);
-
-$getip = "SELECT * FROM development";
-$res = mysqli_query($conn, $getip);
-
-while ($row = mysqli_fetch_assoc($res)) {
-    $ip = $row['ip'];
-}
-
-$titlw = $_POST['title'];
+$title = $_POST['title'];
 $description = $_POST['description'];
 $date = date("M , d , Y");
 $who = $username;
 $code = rand(1000 , 9999);
 
-if (!empty($titlw)) {
+if (!empty($title)) {
     if (!empty($description)) {
         $sql = "INSERT INTO post (code , title , txt , dt , who) VALUES ('$code' , '$titlw' , '$description' , '$date' , '$who')";
 
         mysqli_query($conn, $sql);
         $_SESSION['post'] = 200;
-        header("Location: http://$ip/Atieh/people/agent/new.php");
+        header("Location: http://$ip/Narbon/people/agent/new.php");
     }
 }
 else {
     $_SESSION['post'] = 500;
-    header("Location: http://$ip/Atieh/people/agent/new.php");
+    header("Location: http://$ip/Narbon/people/agent/new.php");
 }
