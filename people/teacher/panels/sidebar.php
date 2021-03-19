4592c0303007f4810cb82020031693b32fe6197b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-20 01:14:56 +0330

    New LI in sidebar-index

--- people/teacher/panels/sidebar.php
+++ people/teacher/panels/sidebar.php
@@ -44,6 +44,11 @@ $result = mysqli_query($conn, $sql);
     <div class="divider"></div>
     <ul class="nav menu">
         <li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+        <li>
+            <a class="" href="tickets.php">
+                <span class="fa fa-envelope">&nbsp;</span> Tickets
+            </a>
+        </li>
         <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
                 <em class="fa fa-navicon">&nbsp;</em> Menu <span data-toggle="collapse" href="#sub-item-1"
                                                                  class="icon pull-right"><em
