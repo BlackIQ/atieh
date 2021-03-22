9dce6fca2dfcb5c9aa9bcc855f19c385f9e8046a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-22 15:05:25 +0430

    Teacher added
    ;

--- people/pack/include/data.php
+++ people/pack/include/data.php
@@ -130,7 +130,21 @@ if ($_SESSION['status'] == true) {
         }
     }
     elseif ($_SESSION['person'] == "teacher") {
-        header("Location: http://$ip/Narbon/people/teacher");
+        $id = $_SESSION['id'];
+
+        $sql = "SELECT * FROM teacher WHERE mcode='$id'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $code = $row["mcode"];
+                $username = $row["username"];
+                $fullname = $row["fname"];
+                $email = $row["email"];
+                $phone = $row["phone"];
+                $institute = $row["icode"];
+            }
+        }
     }
     elseif ($_SESSION['person'] == "agent") {
         header("Location: http://$ip/Narbon/people/agent");
