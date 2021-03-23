51f5c644cf2db06453df1b7e4fb4dd73c0a5de29
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-23 14:31:08 +0430

    Make a check to check people

---
+++ people/student/check.php
@@ -0,0 +1,35 @@
+<?php
+
+session_start();
+
+$server = "localhost";
+$user = "narbon";
+$passwd = "narbon";
+$db = "narbonn";
+
+$conn = mysqli_connect($server, $user, $passwd, $db);
+
+$getip = "SELECT * FROM development";
+$res = mysqli_query($conn, $getip);
+
+while ($row = mysqli_fetch_assoc($res)) {
+    $ip = $row['ip'];
+}
+
+if ($_SESSION['status'] == true && $_SESSION['person'] == "student" ) {
+    
+}
+elseif ($_SESSION['person'] == "teacher") {
+    header("Location: http://$ip/Narbon/people/teacher");
+}
+elseif ($_SESSION['person'] == "agent") {
+    header("Location: http://$ip/Narbon/people/agent");
+}
+elseif ($_SESSION['person'] == "admin") {
+    header("Location: http://$ip/Narbon/people/admin");
+}
+else {
+    header("Location: http://$ip/Narbon");
+}
+
+?>
