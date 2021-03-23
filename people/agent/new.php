a3b123239848145125129daca50b1bb538b1a6f7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-23 16:11:28 +0430

    Fix new.php

--- people/agent/new.php
+++ people/agent/new.php
@@ -1,121 +1,25 @@
 <?php
-include("check.php");
+
+session_start();
+
 include("../pack/include/data.php");
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Narbon - New</title>
-    <link href="../pack/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../pack/css/font-awesome.min.css" rel="stylesheet">
-    <link href="../pack/css/datepicker3.css" rel="stylesheet">
-    <link href="../pack/css/styles.css" rel="stylesheet">
+$title = $_POST['title'];
+$description = $_POST['description'];
+$date = date("M , d , Y");
+$who = $username;
+$code = rand(1000 , 9999);
 
-    <!--Custom Font-->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
-          rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body>
-<?php
-include("../pack/panels/sidebar.php");
-?>
-<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-    <div class="row">
-        <ol class="breadcrumb">
-            <li><a href="../agent">
-                    <em class="fa fa-home"></em>
-                </a></li>
-            <li class="active">New</li>
-        </ol>
-    </div><!--/.row-->
+if (!empty($title)) {
+    if (!empty($description)) {
+        $sql = "INSERT INTO post (code , title , txt , dt , who) VALUES ('$code' , '$title' , '$description' , '$date' , '$who')";
 
-    <div class="row">
-        <div class="col-lg-12">
-            <h1 class="page-header">New Post</h1>
-        </div>
-    </div><!--/.row-->
-
-    <div class="panel panel-container">
-        <?php
-        include("../pack/panels/bar.php");
-        ?>
-    </div>
-
-<div class="row">
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Post
-                    <span class="pull-right clickable panel-toggle panel-button-tab-left">
-                        <em class="fa fa-toggle-up"></em>
-                    </span>
-                </div>
-                <div class="panel-body">
-                    <h1 class="text-warning">Insert a new post</h1>
-                    <hr>
-                    <?php
-                        if ($_SESSION["post"] == "200") {
-                            echo "<h4 class='text-success'><b>> Done !</b></h4>";
-                            $_SESSION['post'] == 0;
-                        }
-                        elseif ($_SESSION["post"] == "500") {
-                            echo "<h4 class='text-danger'><b>> Fill them all !</b></h4>";
-                            $_SESSION['post'] == 0;
-                        }
-                    ?>
-                    <form action="include/new.php" method="POST">
-
-                        <div class="form-group">
-                            <label for="title">Title</label>
-                            <input type="text" class="form-control" name="title" />
-                        </div>
-
-                        <div class="form-group">
-                            <label for="description">Description</label>
-                            <textarea rows="5" class="form-control" name="description" ></textarea>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">Post</button>
-                        </div>
-
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div><!--/.row-->
-
-    <?php
-    include("../pack/panels/footer.php");
-    ?>
-</div>    <!--/.main-->
-
-<script src="../pack/js/jquery-1.11.1.min.js"></script>
-<script src="../pack/js/bootstrap.min.js"></script>
-<script src="../pack/js/chart.min.js"></script>
-<script src="../pack/js/chart-data.js"></script>
-<script src="../pack/js/easypiechart.js"></script>
-<script src="../pack/js/easypiechart-data.js"></script>
-<script src="../pack/js/bootstrap-datepicker.js"></script>
-<script src="../pack/js/custom.js"></script>
-<script>
-    window.onload = function () {
-        var chart1 = document.getElementById("line-chart").getContext("2d");
-        window.myLine = new Chart(chart1).Line(lineChartData, {
-            responsive: true,
-            scaleLineColor: "rgba(0,0,0,.2)",
-            scaleGridLineColor: "rgba(0,0,0,.05)",
-            scaleFontColor: "#c5c7cc"
-        });
-    };
-</script>
-
-</body>
-</html>
+        mysqli_query($conn, $sql);
+        $_SESSION['post'] = 200;
+        header("Location: http://$ip/Narbon/people/agent/post.php");
+    }
+}
+else {
+    $_SESSION['post'] = 500;
+    header("Location: http://$ip/Narbon/people/agent/post.php");
+}
