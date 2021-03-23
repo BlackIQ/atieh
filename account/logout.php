afd4a146cc8e1e92f30b098f1ef0c8eb65b5ba49
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-23 14:53:27 +0430

    Fix logout problems

--- account/logout.php
+++ account/logout.php
@@ -1,6 +1,20 @@
 <?php
 
-include("data.php");
+session_start();
+
+$server = "localhost";
+$user = "narbon";
+$passwd = "narbon";
+$db = "narbonn";
+
+$conn = mysqli_connect($server, $user, $passwd, $db);
+
+$getip = "SELECT * FROM development";
+$res = mysqli_query($conn, $getip);
+
+while ($row = mysqli_fetch_assoc($res)) {
+    $ip = $row['ip'];
+}
 
 session_destroy();
 
