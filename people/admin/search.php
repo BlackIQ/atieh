7cab495ec6c286ca69e2f10a3077af40b2397f7f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-25 16:15:50 +0430

    Fix admin home icon link

--- people/admin/search.php
+++ people/admin/search.php
@@ -30,7 +30,7 @@ include("../pack/panels/sidebar.php");
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
-            <li><a href="../teacher">
+            <li><a href="../admin">
                     <em class="fa fa-home"></em>
                 </a></li>
             <li class="active">Search</li>
