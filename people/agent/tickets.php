abfb47fa75c80d04b7d812a48eeb020a6615666e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-25 16:15:35 +0430

    Fix agent home icon link

--- people/agent/tickets.php
+++ people/agent/tickets.php
@@ -34,7 +34,7 @@ include("../pack/panels/sidebar.php");
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
-            <li><a href="../student">
+            <li><a href="../agent">
                     <em class="fa fa-home"></em>
                 </a></li>
             <li class="active">Tickets</li>
