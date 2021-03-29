9f08c6c418867fad71f2eeaca3246b96165568db
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-03-29 20:23:00 +0430

    Bar created and working on

--- people/pack/panels/bar.php
+++ people/pack/panels/bar.php
@@ -190,5 +190,47 @@ elseif ($_SESSION["person"] == "admin") {
     </div>
     <?php
 }
+elseif ($_SESSION['person'] == "parent") {
+    ?>
+    <div class="row">
+        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+            <div class="panel panel-teal panel-widget border-right">
+                <div class="row no-padding"><em class="fa fa-xl fa-home color-blue"></em>
+                    <div class="large"></div>
+                    <br>
+                    <div class="text-muted"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+            <div class="panel panel-blue panel-widget border-right">
+                <div class="row no-padding"><em class="fa fa-xl fa-clock-o color-orange"></em>
+                    <div class="large"></div>
+                    <br>
+                    <div class="text-muted"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+            <div class="panel panel-orange panel-widget border-right">
+                <div class="row no-padding"><em class="fa fa-xl fa-book color-teal"></em>
+                    <div class="large"></div>
+                    <br>
+                    <div class="text-muted"></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+            <div class="panel panel-red panel-widget ">
+                <div class="row no-padding"><em class="fa fa-xl fa-users color-red"></em>
+                    <div class="large"></div>
+                    <br>
+                    <div class="text-muted"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+}
 
 ?>
