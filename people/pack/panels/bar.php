0e60395443586d23c6f1c0e252ed67c6379d782b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-05 10:22:31 +0430

    idea file

--- people/pack/panels/bar.php
+++ people/pack/panels/bar.php
@@ -47,7 +47,7 @@ if ($_SESSION["person"] == "student") {
 elseif ($_SESSION['person'] == "teacher") {
     ?>
     <div class="row">
-        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="col-xs-6 col-md-4 col-lg-4 no-padding">
             <div class="panel panel-teal panel-widget border-right">
                 <div class="row no-padding"><em class="fa fa-xl fa-home color-blue"></em>
                     <div class="large">8</div>
@@ -56,7 +56,7 @@ elseif ($_SESSION['person'] == "teacher") {
                 </div>
             </div>
         </div>
-        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="col-xs-6 col-md-4 col-lg-4 no-padding">
             <div class="panel panel-blue panel-widget border-right">
                 <div class="row no-padding"><em class="fa fa-xl fa-clock-o color-orange"></em>
                     <div class="large">12 H</div>
@@ -65,16 +65,7 @@ elseif ($_SESSION['person'] == "teacher") {
                 </div>
             </div>
         </div>
-        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
-            <div class="panel panel-orange panel-widget border-right">
-                <div class="row no-padding"><em class="fa fa-xl fa-book color-teal"></em>
-                    <div class="large"></div>
-                    <br>
-                    <div class="text-muted"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+        <div class="col-xs-6 col-md-4 col-lg-4 no-padding">
             <div class="panel panel-red panel-widget ">
                 <div class="row no-padding"><em class="fa fa-xl fa-users color-red"></em>
                     <div class="large">54</div>
