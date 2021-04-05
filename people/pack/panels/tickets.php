b7217f81c90dc9a61bc57c1b07e2909cc122bc9d
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-05 10:27:11 +0430

    Fix tickts panel condition

--- people/pack/panels/tickets.php
+++ people/pack/panels/tickets.php
@@ -1,41 +1,40 @@
-<table class="table table-hover table-responsive table-bordered">
-    <thead>
-        <tr>
-            <th scope="col">Code</th>
-            <th scope="col">Title</th>
-            <th scope="col">Date</th>
-            <th scope="col">Status</th>
-        </tr>
-    </thead>
-    <tbody>
-
-        <?php
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                ?>
-                    <tr title="<?php echo $row["answer"]; ?>" data-toggle="tooltip" data-placement="right">
-                        <th scope="row"><?php echo $row['code']; ?></th>
-                        <td><?php echo $row['title']; ?></td>
-                        <td><?php echo $row["dt"]; ?></td>
-                        <td class="text-danger">
-                            <?php
-                                if ($row['answer'] == 'no') {
-                                    echo '<span class="text-danger">No Answer</span>';
-                                }
-                                else {
-                                    $answer = $row['answer'];
-                                    echo '<span class="text-success">'.$answer.'</span>';
-                                }
-                            ?>
-                        </td>
-                    </tr>
-                <?php
-            }
-        }
-        else {
-            echo "<h1>You have no tickets yet.</h1>";
-        }
-
+<?php
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
         ?>
-    </tbody>
-</table>
+        <table class="table table-hover table-responsive table-bordered">
+            <thead>
+            <tr>
+                <th scope="col">Code</th>
+                <th scope="col">Title</th>
+                <th scope="col">Date</th>
+                <th scope="col">Status</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr title="<?php echo $row["answer"]; ?>" data-toggle="tooltip" data-placement="right">
+                    <th scope="row"><?php echo $row['code']; ?></th>
+                    <td><?php echo $row['title']; ?></td>
+                    <td><?php echo $row["dt"]; ?></td>
+                    <td class="text-danger">
+                        <?php
+                            if ($row['answer'] == 'no') {
+                                echo '<span class="text-danger">No Answer</span>';
+                            }
+                            else {
+                                $answer = $row['answer'];
+                                echo '<span class="text-success">'.$answer.'</span>';
+                            }
+                        ?>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <?php
+    }
+}
+else {
+    echo "<h1>You have no tickets yet.</h1>";
+}
+
+?>
