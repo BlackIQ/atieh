8cbc184ec61454a071a6390336dde416217bcdf2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-04-05 10:29:53 +0430

    Working on tickets

--- people/pack/panels/tickets.php
+++ people/pack/panels/tickets.php
@@ -1,40 +1,43 @@
 <?php
 if (mysqli_num_rows($result) > 0) {
+    ?>
+    <table class="table table-hover table-responsive table-bordered">
+        <thead>
+        <tr>
+            <th scope="col">Code</th>
+            <th scope="col">Title</th>
+            <th scope="col">Date</th>
+            <th scope="col">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+    <?php
     while ($row = mysqli_fetch_assoc($result)) {
         ?>
-        <table class="table table-hover table-responsive table-bordered">
-            <thead>
-            <tr>
-                <th scope="col">Code</th>
-                <th scope="col">Title</th>
-                <th scope="col">Date</th>
-                <th scope="col">Status</th>
+            <tr title="<?php echo $row["answer"]; ?>" data-toggle="tooltip" data-placement="right">
+                <th scope="row"><?php echo $row['code']; ?></th>
+                <td><?php echo $row['title']; ?></td>
+                <td><?php echo $row["dt"]; ?></td>
+                <td class="text-danger">
+                    <?php
+                        if ($row['answer'] == 'no') {
+                            echo '<span class="text-danger">No Answer</span>';
+                        }
+                        else {
+                            $answer = $row['answer'];
+                            echo '<span class="text-success">'.$answer.'</span>';
+                        }
+                    ?>
+                </td>
             </tr>
-            </thead>
-            <tbody>
-                <tr title="<?php echo $row["answer"]; ?>" data-toggle="tooltip" data-placement="right">
-                    <th scope="row"><?php echo $row['code']; ?></th>
-                    <td><?php echo $row['title']; ?></td>
-                    <td><?php echo $row["dt"]; ?></td>
-                    <td class="text-danger">
-                        <?php
-                            if ($row['answer'] == 'no') {
-                                echo '<span class="text-danger">No Answer</span>';
-                            }
-                            else {
-                                $answer = $row['answer'];
-                                echo '<span class="text-success">'.$answer.'</span>';
-                            }
-                        ?>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
         <?php
     }
+    ?>
+        </tbody>
+    </table>
+    <?php
 }
 else {
     echo "<h1>You have no tickets yet.</h1>";
 }
-
 ?>
