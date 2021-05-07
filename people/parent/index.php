ee84b6123bbc16013af54149f915e8e600109d41
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-07 11:24:25 +0430

    Fixing parent index linking

--- people/parent/index.php
+++ people/parent/index.php
@@ -12,8 +12,8 @@ $icode = $_SESSION['icode'];
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Narbon - Dashboard</title>
-    <link href="../pack/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../pack/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://office.narbon.ir:4488/pack/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://office.narbon.ir:4488/pack/css/font-awesome.min.css" rel="stylesheet">
     <link href="../pack/css/datepicker3.css" rel="stylesheet">
     <link href="../pack/css/styles.css" rel="stylesheet">
 
@@ -118,14 +118,14 @@ include("../pack/panels/sidebar.php");
     ?>
 </div>    <!--/.main-->
 
-<script src="../pack/js/jquery-1.11.1.min.js"></script>
-<script src="../pack/js/bootstrap.min.js"></script>
-<script src="../pack/js/chart.min.js"></script>
-<script src="../pack/js/chart-data.js"></script>
-<script src="../pack/js/easypiechart.js"></script>
-<script src="../pack/js/easypiechart-data.js"></script>
-<script src="../pack/js/bootstrap-datepicker.js"></script>
-<script src="../pack/js/custom.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/jquery-1.11.1.min.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/bootstrap.min.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/chart.min.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/chart-data.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/easypiechart.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/easypiechart-data.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/bootstrap-datepicker.js"></script>
+<script src="http://office.narbon.ir:4488/pack/js/custom.js"></script>
 <script>
     window.onload = function () {
         var chart1 = document.getElementById("line-chart").getContext("2d");
