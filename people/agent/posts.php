28689a0fddca49ea76b41fe913f9b7913de3a580
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-08 08:26:51 +0430

    Remove condition of style for agent posts

--- people/agent/posts.php
+++ people/agent/posts.php
@@ -21,10 +21,6 @@ $result = mysqli_query($conn, $sql);
     <!--Custom Font-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <?php
