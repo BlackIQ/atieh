4f67448ee9936423a764db441e2748f68e68c716
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-08 08:32:21 +0430

    Remove condition of style for teacher class

--- people/teacher/class.php
+++ people/teacher/class.php
@@ -38,10 +38,6 @@ if ($sex == 'male') {
     <!--Custom Font-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <?php
