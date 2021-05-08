c0a4b540c4b01e5e4e6fba9e062e1503da4d7116
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-05-08 08:30:53 +0430

    Remove condition of style for student payment

--- people/student/payment.php
+++ people/student/payment.php
@@ -19,10 +19,6 @@ include("../pack/include/data.php");
     <!--Custom Font-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet">
-    <!--[if lt IE 9]>
-    <script src="js/html5shiv.js"></script>
-    <script src="js/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
 <?php
