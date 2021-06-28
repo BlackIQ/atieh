b1fae152ffba9431777b0ed77bc166874b4c89b2
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-29 01:37:06 +0430

    Fix logout

--- account/logout.php
+++ account/logout.php
@@ -2,20 +2,10 @@
 
 session_start();
 
-$server = "localhost";
-$user = "narbon";
-$passwd = "narbon";
-$db = "narbonn";
-
-$conn = mysqli_connect($server, $user, $passwd, $db);
-
-$getip = "SELECT * FROM development";
-$res = mysqli_query($conn, $getip);
-
-while ($row = mysqli_fetch_assoc($res)) {
-    $ip = $row['ip'];
-}
-
 session_destroy();
 
-header("Location: http://$ip/Narbon");
+?>
+<script>
+    window.location.replace("../")
+</script>
+<?php
