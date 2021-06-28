864118cae79bb984c0799e81b60b2c0d308bba1f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-29 00:56:42 +0430

    Create main config template

---
+++ pack/config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Here you have to set MSI ( MySQL Server IP )
+$msi = "";
+
+// Here you have to set MSU ( MySQL Server User )
+$msu = "";
+
+// Here you have to set MSP ( MySQL Server Password )
+$msp = "";
+
+// Here you have to set MSD ( MySQL Server Database )
+$msd = "";
+
+?>
