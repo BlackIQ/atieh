b2f5938b17039c8fe9dfcfd5fe58f1ddb399094e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-29 22:18:08 +0430

    Fix links

--- people/student/index.php
+++ people/student/index.php
@@ -48,9 +48,9 @@ include("../pack/include/data.php");
     <div class="divider"></div>
     <ul class="nav menu">
         <li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
-        <li class=""><a href="index.php"><em class="fa fa-home">&nbsp;</em> Class</a></li>
-        <li class=""><a href="index.php"><em class="fa fa-credit-card-alt">&nbsp;</em> Payment</a></li>
-        <li class=""><a href="index.php"><em class="fa fa-id-card">&nbsp;</em> Profile</a></li>
+        <li class=""><a href="class.php"><em class="fa fa-home">&nbsp;</em> Class</a></li>
+        <li class=""><a href="payment.php"><em class="fa fa-credit-card-alt">&nbsp;</em> Payment</a></li>
+        <li class=""><a href="profile.php"><em class="fa fa-id-card">&nbsp;</em> Profile</a></li>
     </ul>
 </div>
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
