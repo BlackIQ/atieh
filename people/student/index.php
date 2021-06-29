732d45dca959ef4c0e7d878e2c4696ae31d7a426
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-29 22:02:01 +0430

    Working on sidebar and others

--- people/student/index.php
+++ people/student/index.php
@@ -1,7 +1,4 @@
 <?php
-
-include('check.php');
-
 include("../pack/include/data.php");
 ?>
 
@@ -17,9 +14,42 @@ include("../pack/include/data.php");
     <link href="../../pack/css/styles.css" rel="stylesheet">
 </head>
 <body>
-<?php
-include("../pack/panels/sidebar.php");
-?>
+<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#sidebar-collapse"><span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span></button>
+            <a class="navbar-brand" href="http://<?php echo $ip; ?>/Narbon"><span>Narbon </span>Project</a>
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown"><a class="dropdown-toggle count-info" href="http://<?php echo $ip; ?>/Narbon/account/logout.php">
+                        <em class="fa fa-sign-out"></em>
+                    </a>
+            </ul>
+        </div>
+    </div><!-- /.container-fluid -->
+</nav>
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+    <div class="profile-sidebar">
+        <div class="profile-userpic">
+            <img src="../pack/pic.jpeg" class="img-responsive" alt="User Image">
+        </div>
+        <div class="profile-usertitle">
+            <div class="profile-usertitle-name"><?php echo $username; ?></div>
+            <div class="profile-usertitle-status"><span class="indicator label-success"></span>Student</div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class="divider"></div>
+    <ul class="nav menu">
+        <li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+        <li class=""><a href="index.php"><em class="fa fa-home">&nbsp;</em> Class</a></li>
+        <li class=""><a href="index.php"><em class="fa fa-credit-card-alt">&nbsp;</em> Payment</a></li>
+        <li class=""><a href="index.php"><em class="fa fa-id-card">&nbsp;</em> Profile</a></li>
+    </ul>
+</div>
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
@@ -35,12 +65,47 @@ include("../pack/panels/sidebar.php");
         </div>
     </div>
     <div class="panel panel-container">
-        <?php
-        include("../pack/panels/bar.php");
-        ?>
+        <div class="row">
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-teal panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-money color-blue"></em>
+                        <div class="large"><?php echo $payment; ?></div>
+                        <br>
+                        <div class="text-muted">Payment Status</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-blue panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-clock-o color-orange"></em>
+                        <div class="large"><?php echo $start; ?></div>
+                        <br>
+                        <div class="text-muted">Start Time</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-orange panel-widget border-right">
+                    <div class="row no-padding"><em class="fa fa-xl fa-book color-teal"></em>
+                        <div class="large"><?php echo $level; ?></div>
+                        <br>
+                        <div class="text-muted">Level</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-6 col-md-3 col-lg-3 no-padding">
+                <div class="panel panel-red panel-widget ">
+                    <div class="row no-padding"><em class="fa fa-xl fa-user color-red"></em>
+                        <div class="large"><?php echo $teacher; ?></div>
+                        <br>
+                        <div class="text-muted">Teacher</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     My Class
@@ -49,10 +114,8 @@ include("../pack/panels/sidebar.php");
                     </span>
                 </div>
                 <div class="panel-body">
-                    <h1 class="text-warning">Class Information</h1>
-                    <hr>
-                    <h4 class="text-primary">Teacher's name</h4>
-                    <p class="text-success"><b><i class="fa fa-user"></i> <?php echo $fteacher; ?></b></p>
+                    <h4>Teacher</h4>
+                    <p><b><?php echo $fteacher; ?></b></p>
                     <br>
                     <h4 class="text-primary"><i class="fa fa-link"></i> Class links</h4>
                     <p class="text-danger"><b>
@@ -83,9 +146,19 @@ include("../pack/panels/sidebar.php");
             </div>
         </div>
     </div>
-    <?php
-    include("../pack/panels/footer.php");
-    ?>
+    <div class="row">
+        <div class="col-sm-12">
+            <p class="back-link"><a href="http://<?php echo $ip; ?>/Narbon">Narbon</a></p>
+            <p class="back-link">Created by <a href="https://www.github.com/BlackIQ">Amirhossein Mohammadi</a></p>
+            <p class="back-link">Powered By <a href="https://www.linkedin.com/company/neotrinost">Neotrinost</a> <i class="fa fa-copyright"></i> <?php echo date("Y"); ?></p>
+            <p class="back-link">
+                <i class="fa fa-lg fa-linkedin-square text-info"></i>
+                <i class="fa fa-lg fa-github"></i>
+                <i class="fa fa-lg fa-telegram text-primary"></i>
+                <i class="fa fa-lg fa-instagram text-danger"></i>
+            </p>
+        </div>
+    </div>
 </div>
 <script src="../../pack/js/jquery-1.11.1.min.js"></script>
 <script src="../../pack/js/bootstrap.min.js"></script>
