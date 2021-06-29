f645dba0f4153a74b011d56361e145456db8a976
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-29 22:31:13 +0430

    Add bar

--- people/student/index.php
+++ people/student/index.php
@@ -56,9 +56,11 @@ include("../pack/include/data.php");
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
-            <li><a href="../student">
-                <em class="fa fa-home"></em>
-            </a></li>
+            <li>
+                <a href="../student">
+                    <em class="fa fa-home"></em>
+                </a>
+            </li>
             <li class="active">Dashboard</li>
         </ol>
     </div>
