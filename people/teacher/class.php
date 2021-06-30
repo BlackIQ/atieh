f6e9a058314e8d0569b4e3b396d9f4b760cb9f38
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-06-30 21:42:43 +0430

    Add sidebar and fix active item

--- people/teacher/class.php
+++ people/teacher/class.php
@@ -55,6 +55,58 @@ if ($sex == 'male') {
         </div>
     </div>
 </nav>
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+    <div class="profile-sidebar">
+        <div class="profile-userpic">
+            <img src="../pack/pic.jpeg" class="img-responsive" alt="User Image">
+        </div>
+        <div class="profile-usertitle">
+            <div class="profile-usertitle-name"><?php echo $username; ?></div>
+            <div class="profile-usertitle-status"><span class="indicator label-success"></span>Teacher</div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class="divider"></div>
+    <ul class="nav menu">
+        <li class=""><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+        <li class="parent active">
+            <a data-toggle="collapse" href="#sub-item-1">
+                <em class="fa fa-navicon">&nbsp;</em> Classes
+                <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right">
+                    <em class="fa fa-plus"></em>
+                </span>
+            </a>
+            <ul class="children collapse" id="sub-item-1">
+                <?php
+                $sql = "SELECT * FROM class WHERE tcode='$id'";
+                $result = mysqli_query($conn, $sql);
+
+                if (mysqli_num_rows($result) > 0) {
+                    // output data of each row
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        if ($row["sex"] == "male") {
+                            $icon = "male";
+                        }
+                        elseif ($row["sex"] == "female") {
+                            $icon = "female";
+                        }
+                        else {
+                            $icon = "times";
+                        }
+                        ?>
+                        <li>
+                            <a class="" href="class.php?code=<?php echo $row["ccode"]; ?>">
+                                <span class="fa fa-<?php echo $icon; ?>">&nbsp;</span> <?php echo $row["level"]; ?>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                }
+                ?>
+            </ul>
+        </li>
+    </ul>
+</div>
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
