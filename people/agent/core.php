ad83b97da3207133d99e850bf7db6a2758a99dde
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-01 23:50:31 +0430

    Fix core

---
+++ people/agent/core.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$server = "localhost";
+$user = "narbon";
+$passwd = "narbon";
+$db = "narbonn";
+
+$conn = mysqli_connect($server, $user, $passwd, $db);
+
+$id = $_SESSION['id'];
+
+$sql = "SELECT * FROM agent WHERE mcode='$id'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $code = $row["mcode"];
+        $username = $row["username"];
+        $fullname = $row["fname"];
+        $email = $row["email"];
+        $phone = $row["phone"];
+    }
+}
