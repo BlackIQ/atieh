a0bde25c925035f23f3e9d9f734b74c8c4e2e1b5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-02 00:44:21 +0430

    Add sidebar for index

--- people/agent/posts.php
+++ people/agent/posts.php
@@ -39,6 +39,23 @@ $result = mysqli_query($conn, $sql);
         </div>
     </div>
 </nav>
+<div id="sidebar-collapse" class="col-sm-3 col-lg-2 sidebar">
+    <div class="profile-sidebar">
+        <div class="profile-userpic">
+            <img src="../pack/pic.jpeg" class="img-responsive" alt="User Image">
+        </div>
+        <div class="profile-usertitle">
+            <div class="profile-usertitle-name"><?php echo $username; ?></div>
+            <div class="profile-usertitle-status"><span class="indicator label-success"></span>Agent</div>
+        </div>
+        <div class="clear"></div>
+    </div>
+    <div class="divider"></div>
+    <ul class="nav menu">
+        <li class="active"><a href="index.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+        <li class=""><a href="posts.php"><em class="fa fa-book">&nbsp;</em> Posts</a></li>
+    </ul>
+</div>
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
     <div class="row">
         <ol class="breadcrumb">
