205a9ddd091157cafe5204d0e9c9419201a9d77b
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-31 13:20:29 +0430

    Start some backend for index.php

---
+++ new/auth/index.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+include("auth.php");
+include("../pack/config/configuration.php");
+
+if ($_SESSION['session_status']) {
+    $role = $_SESSION['user_role'];
+    ?>
+    <script>
+        window.location.replace("../people/<?php echo $role; ?>");
+    </script>
+    <?php
+}
+
+?>
