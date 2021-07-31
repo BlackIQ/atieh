99bdd9e731de8f4fce6f300013310b386f9aa98f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-31 17:41:40 +0430

    All done in index

--- new/index.php
+++ new/index.php
@@ -4,6 +4,9 @@ session_start();
 
 include("pack/config/configuration.php");
 
+$get_posts_query = "";
+$result_posts_query = mysqli_query($connection, $get_posts_query);
+
 ?>
 
 <!DOCTYPE html>
@@ -69,20 +72,63 @@ include("pack/config/configuration.php");
     </div>
 </nav>
 <div class="container">
-
+    <div class="row">
+        <div class="col-md-8">
+            <br>
+            <?php
+            if (mysqli_num_rows($result_posts_query) > 1) {
+                while ($post_array = mysqli_fetch_assoc($result_posts_query)) {
+                    ?>
+                    <div class="card mb-4">
+                        <div class="card-body" style="padding: 5%;">
+                            <h2 class="card-title"><?php echo $post_array['title']; ?></h2>
+                            <hr>
+                            <p class="card-text"><?php echo $post_array['post']; ?></p>
+                        </div>
+                        <div class="card-footer text-muted" style="padding: 2%;">
+                            <?php echo "Posted on " . $post_array['dt'] . " By " . $post_array['person']; ?>
+                        </div>
+                    </div>
+                    <?php
+                }
+            }
+            else {
+                echo "<h2>No posts yet.</h2>";
+            }
+            ?>
+        </div>
+        <div class="col-md-4">
+            <div class="card my-4">
+                <h5 class="card-header" style="padding: 5%;">Sidebar 1</h5>
+                <div class="card-body">
+                    <p>Institute is always open</p>
+                    <p>Institute is always open</p>
+                    <p>Institute is always open</p>
+                </div>
+            </div>
+            <div class="card my-4">
+                <h5 class="card-header" style="padding: 5%;">Sidebar 2</h5>
+                <div class="card-body">
+                    <p>Institute is always open</p>
+                    <p>Institute is always open</p>
+                    <p>Institute is always open</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <footer class="py-5 bg-dark">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
                 <div class="m-0 text-left text-white">
-                    <h4 class="text-primary">Atieh</h4>
-                    <br>
-                    <p>
+                    <p class="text-primary">Atieh</p>
+                    <small>
                         Atieh institute.
                         <br>
                         blah blah blah blah blah blah.
-                    </p>
+                    </small>
+                    <br>
                     <br>
                     <p>
                         <a class="link" target="_blank" href="#">
@@ -96,19 +142,18 @@ include("pack/config/configuration.php");
                         <a class="link" target="_blank" href="#">
                             <i class="fa fa-whatsapp"></i>
                         </a>
-
                     </p>
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="m-0 text-left text-white">
-                    <h4 class="text-primary"><a href="https://blackiq.ir">Amirhossein Mohammadi</a></h4>
-                    <br>
-                    <p>
+                    <p class="text-primary"><a href="https://blackiq.ir">Amirhossein Mohammadi</a></p>
+                    <small>
                         Hi, I am Amir. Php and Python developer.
                         <br>
                         Artificial intelligence (AI) student.
-                    </p>
+                    </small>
+                    <br>
                     <br>
                     <p>
                         <a class="link" target="_blank" href="https://t.me/BlackIQ">
@@ -135,13 +180,13 @@ include("pack/config/configuration.php");
             </div>
             <div class="col-md-4">
                 <div class="m-0 text-left text-white">
-                    <h4 class="text-primary"><a href="https://neotrinost.ir">Neotrinost</a></h4>
-                    <br>
-                    <p>
+                    <p class="text-primary"><a href="https://neotrinost.ir">Neotrinost</a></p>
+                    <small>
                         We are development company that develop Android, Desktop and Web based applications.
                         <br>
                         But there is a bit difference too, our other activity is to accept you projects.
-                    </p>
+                    </small>
+                    <br>
                     <br>
                     <p>
                         <a class="link" target="_blank" href="https://t.me/neotrinost">
