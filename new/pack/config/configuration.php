ae3171630d1141196d31a84d5194ec6d6f23432a
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-31 12:31:09 +0430

    Creating config file

---
+++ new/pack/config/configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+// Hosting IP
+$host = "";
+
+// MariaDB Config
+$server = "";
+$user = "";
+$password = "";
+$database = "";
+
+// Mail Config
+$mail_username = "";
+$mail_password = "";
+
+// MariaDB Connection
+$connection = mysqli_connect($server, $user, $password, $database);
