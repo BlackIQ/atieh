420dd5079c482e6369e4de8cb849816c2c9de913
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-07-31 14:47:24 +0430

    Add check admin

---
+++ new/user/admin/index.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+include("../../pack/config/configuration.php");
+
+if ($_SESSION['session_status'] != true) {
+    ?>
+    <script>
+        window.location.replace("../../");
+    </script>
+    <?php
+}
+if ($_SESSION['user_role'] != "admin") {
+    $role = $_SESSION['user_role'];
+    ?>
+    <script>
+        window.location.replace("../<?php echo $role; ?>");
+    </script>
+    <?php
+}
+
+?>
